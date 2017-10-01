7467f7b64afe308f00eb1fa4713960106c5c0e68
guxi <guxi@live.com>
2017-10-01 11:38:39 +0800

    0.2

--- CourseDemo/src/edu/wust/hello/hi2.java
+++ CourseDemo/src/edu/wust/hello/hi2.java
@@ -1,10 +1,16 @@
 package edu.wust.hello;
 
 public class hi2 {
-	 private String s; 
-     static int i=10;	
-     public void setS(String t)
-          {     System.out.println(""+t);     }
-     public static String getS()
-     {	return ""+i;     }
+	static int i = 10;
+
+	public void setS(String t) {
+		System.out.println("" + t);
+	}
+
+	// 静态属性和方法
+	private String s;
+
+	public static String getS() {
+		return "" + i;
+	}
 }
